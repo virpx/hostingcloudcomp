d0eb859fc0948b4ab47343957a9e67eb680a3dda
virpx <vierypangeran@gmail.com>
2025-01-29 00:06:16 +0700

    update

--- src/slices/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import resetPassword from "./resetPassword";
-
-const store = configureStore({
-  reducer: {
-    resetPassword: resetPassword,
-  },
-});
-export default store;
